b830e1cfb516665f18a9c4f8273bb8cfe80d785b
mouag <mo.ouag@gmail.com>
2021-11-14 12:32:08 +0100

    logging

--- vars/log.groovy
+++ vars/log.groovy
@@ -4,21 +4,19 @@ def call(def msg){
 
 
 def info(def msg) {
-    ansiColor('xterm') {
+
         echo "${getGreen}${msg}";
-    }
+
 }
 
 def warn(def msg) {
-    ansiColor('xterm') {
         echo "${getCyan}${msg}";
-    }
 }
 
 def error(def msg) {
-    ansiColor('xterm') {
+
         echo "${getRed}${msg}";
-    }
+
 }
 
 def getRed() { "\u001B[31m" }
