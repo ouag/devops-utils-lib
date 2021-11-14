595ed512b5759ea4c4a8ab61ae623acd0b4904e5
mouag <mo.ouag@gmail.com>
2021-11-14 20:34:23 +0100

    logging

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,22 +1,5 @@
-def call(def msg){
+def call(def msg) {
     echo "${getGreen}${msg}";
-}
-
-
-def info(def msg) {
-
-        echo "${Green}${msg}";
-
-}
-
-def warn(def msg) {
-        echo "${Cyan}${msg}";
-}
-
-def error(def msg) {
-
-        echo "${Red}${msg}";
-
 }
 
 def getRed() { "\u001B[31m" }
@@ -24,3 +7,21 @@ def getRed() { "\u001B[31m" }
 def getGreen() { "\u001B[32m" }
 
 def getCyan() { "\u001B[36m" }
+
+def info(def msg) {
+
+    echo "${getGreen}${msg}";
+
+}
+
+def warn(def msg) {
+    echo "${getCyan}${msg}";
+}
+
+def error(def msg) {
+
+    echo "${getRed}${msg}";
+
+}
+
+
