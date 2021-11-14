44395ff0bce65436e782ab24ba66d5583840a2d4
mouag <mo.ouag@gmail.com>
2021-11-14 11:46:43 +0100

    adding structure

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,3 +1,28 @@
 def call(){
-    println("hello freind ¡¡¡")
+
 }
+
+
+def info(def msg) {
+    ansiColor('xterm') {
+        echo "${getGreen}${msg}"
+    }
+}
+
+def warn(def msg) {
+    ansiColor('xterm') {
+        echo "${getCyan}${msg}"
+    }
+}
+
+def error(def msg) {
+    ansiColor('xterm') {
+        echo "${getRed}${msg}"
+    }
+}
+
+def getRed() { "\u001B[31m" }
+
+def getGreen() { "\u001B[32m" }
+
+def getCyan() { "\u001B[36m" }
